7e17626a112d9f98a0b69653fe10b379df57f95a
bfrazier <bthehippie2@gmail.com>
2022-11-30 16:01:30 -0500

    made button log to console when clicked

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,9 @@ let searchBar = document.getElementById('search-bar');
 let searchBtn = document.getElementById('searchBtn');
 let historyUl = document.getElementById('history-list');
 let forecast = document.getElementById('forecast');
+let searchHistory = document.getElementById('search-history');
+
+searchBtn.addEventListener('click', function(){
+    console.log('click');
+});
 
