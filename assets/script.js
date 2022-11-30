04c74ea431c9807ff9116c6f3cb132a13fd1820a
bfrazier <bthehippie2@gmail.com>
2022-11-30 15:34:36 -0500

    linked elements to script

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+let searchBar = document.getElementById('search-bar');
+let searchBtn = document.getElementById('searchBtn');
+let historyUl = document.getElementById('history-list');
+let forecast = document.getElementById('forecast');
+
