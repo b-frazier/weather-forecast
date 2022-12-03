2900fe7dbbfc243596b5f2d7803d398239f183d8
bfrazier <bthehippie2@gmail.com>
2022-12-02 21:36:18 -0500

    used api documentation from rapid api

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,31 @@ let historyUl = document.getElementById('history-list');
 let forecast = document.getElementById('forecast');
 let searchHistory = document.getElementById('search-history');
 
+
+
 searchBtn.addEventListener('click', function(){
-    const city = searchBar.value;
+    let city = searchBar.value;
     console.log(city);
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'd7c6b4bfefmsh543bab1a89406f9p15f96bjsn733aef5dbccc',
+            'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
+        }
+    };
+    
+    function getWeather(){
+        fetch('https://open-weather13.p.rapidapi.com/city/'+city, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+    };
+
+    getWeather();
 });
+
+
+
+
 
