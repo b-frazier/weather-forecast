61545bbe8897643aec6c34c5c8cb769e54f2daeb
bfrazier <bthehippie2@gmail.com>
2022-12-08 11:44:39 -0600

    weather object logs to console

--- assets/script.js
+++ assets/script.js
@@ -3,35 +3,41 @@ let searchBtn = document.getElementById('searchBtn');
 let historyUl = document.getElementById('history-list');
 let forecast = document.getElementById('forecast');
 let searchHistory = document.getElementById('search-history');
-
-
-
-/* searchBtn.addEventListener('click', function(){
-    
-});
-*/
-
 let key = '567de4c5feb717e35af6cd339ab5f2d9';
-let city = 'columbus';
+let lat = null;
+let lon = null;
 
-function cityLocation(){
+
+searchBtn.addEventListener('click', function(){
+    getCityLocation(searchBar.value);
+});
+
+// converts city name to coordinates
+function getCityLocation(city){
     fetch('http://api.openweathermap.org/geo/1.0/direct?q='+ city +'&limit=2&appid='+ key)
     .then(response => response.json())
-    .then (function(data){
+    .then(data => {
         let lat = data[0].lat;
         let lon = data[0].lon;
-        let cityName = data[0].name;
-        getWeather(lat, lon, cityName);
-    })
-};
-function getWeather(lat, lon, cityName){
-    fetch('http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + key)
+        return getWeather(lat, lon);
+    }) 
     .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+    .then(forecast => {
+        console.log(forecast)
+    })
+    .catch(err => console.error());
 };
 
-cityLocation();
+// take coordinates, output weather
+function getWeather(lat, lon){
+   return fetch('http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + key + '&units=imperial')
+};
+
+function findTemp(data){
+    for (let i = 0; i < data.length; i+= 8){
+        forecast.textContent = data[i].list[0].main.temp;
+    }
+}
 
 
 
